0574d936372a069651e2fd0f31fa1e32a164b4b1
Julia Lojek <julialojek@gmail.com>
2020-05-26 17:30:36 +0200

    class-transformer

--- src/app.ts
+++ src/app.ts
@@ -10,4 +10,25 @@ new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
 
+// lodash with types
 console.log(_.shuffle([3, 5, 1, 8]));
+
+// class-transformer
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import Product from "./models/product";
+
+const products = [
+  // imitates fetched data
+  { title: "book", price: 10.99 },
+  { title: "phone", price: 139.99 },
+];
+
+// manually rewriting fetched data to our ts model
+// const loadedProducts = products.map((product) => {
+//   return new Product(product.title, product.price);
+// });
+
+const loadedProducts = plainToClass(Product, products); // it goes through the entire array and convert its elements to the class model provided in the first argument
+
+loadedProducts.forEach((product) => console.log(product.getInfo()));
